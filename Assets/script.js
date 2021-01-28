1a3f46bc6a9126a8a4f46c9ebee426aaf9cb22fe
Leandro Parrado <lparrado321@gmail.com>
2021-01-27 19:36:59 -0500

    Search button finally works. Input is console logged.

--- Assets/script.js
+++ Assets/script.js
@@ -3,11 +3,10 @@ $(document).ready(function () {
     //This is our API key
     const APIkey = "f23a679e1f1c0c137cb62393cae527e6";
 
-    //Here we are building the URL we need to query the database
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIkey;
-
     //Returns search button
     $("#search-button").on("click", function() {
+
+        //Define searchValue
         var searchValue = $("#search-value").val()
         console.log(searchValue);
 
@@ -20,6 +19,9 @@ $(document).ready(function () {
 
     
     function searchWeather(searchValue) {
+
+         //Here we are building the URL we need to query the database
+       var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIkey;
 
         //Here we run our AJAX call to the OpenWeatherMap API
         $.ajax({
